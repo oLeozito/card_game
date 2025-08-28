15daecf89c4fd974da62555b285a62f73931c95a
Leonardo <leonardooliver222@hotmail.com>
2025-08-27 22:48:07 -0300

    Adicionei menu simples generalizando codigo

--- cliente.go
+++ cliente.go
@@ -6,6 +6,8 @@ import (
 	"time"
 	"strings"
 	"bufio"
+	"io"
+	"os"
 )
 
 func main() {
@@ -30,8 +32,64 @@ func main() {
 	// Cria o writer e reader para a conexao
 	writer := bufio.NewWriter(conn)
 	reader := bufio.NewReader(conn)
-
 	
+	go interpreter(reader)
+
+	// Loop do Menu pra o usuario ja generalizando
+	for {
+		fmt.Println("\nEscolha uma opção:")
+		fmt.Println("1. Entrar em Sala")
+		fmt.Println("2. Criar nova sala privada")
+		fmt.Println("3. Mensagem Global")
+		fmt.Println("0. Sair")
+		fmt.Printf("> ")
+
+		// Lê a entrada do usuário do terminal
+		userInputReader := bufio.NewReader(os.Stdin)
+		input, _ := userInputReader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		
+		case "1":
+			local_r := bufio.NewReader(os.Stdin)
+
+			fmt.Printf("Quer entrar numa sala privada ou publica?\n1-Publica\n2-Privada\n>")
+			opcao, _ := local_r.ReadString('\n')
+			opcao = strings.TrimSpace(opcao)
+
+			switch opcao{
+			case "1":
+				// CHAMA O BAGULHO QUE ENVIA A MENSAGEM PRO SERVIDOR
+			case "2":
+				// CHAMA O BAGULHO QUE ENVIA A MENSAGEM PRO SERVIDOR
+			default:
+				fmt.Println("Opção inválida. Tente novamente.")
+			}
+
+		case "2":
+			fmt.Println("Criando Sala Privada")
+		
+		case "3":
+			fmt.Printf("Digite sua Mensagem:\n> ")
+			// Usa o leitor do terminal para ler a mensagem
+			message, _ := userInputReader.ReadString('\n')
+			message = strings.TrimSuffix(message, "\n")
+			
+			// O print agora funcionará, pois você leu a entrada do terminal
+			fmt.Println("So de teste aqui: ", message)
+
+		case "0":
+			// Envia uma mensagem para o servidor informando que o cliente está saindo
+			writer.WriteString("QUIT:\n")
+			writer.Flush()
+			fmt.Println("Saindo do jogo. Desconectando...")
+			time.Sleep(1 * time.Second)
+			return // Sai da goroutine principal e fecha a conexao
+		default:
+			fmt.Println("Opção inválida. Tente novamente.")
+		}
+	}
 
 
 }
